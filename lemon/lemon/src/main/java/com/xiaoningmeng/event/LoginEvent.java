058b838d711312a98468a29bd91bf3cf6f913a96
gaoyong <gaoyong06@qq.com>
2016-01-19 16:04:02 +0800

    增加:构造函数

--- lemon/lemon/src/main/java/com/xiaoningmeng/event/LoginEvent.java
+++ lemon/lemon/src/main/java/com/xiaoningmeng/event/LoginEvent.java
@@ -1,5 +1,11 @@
 package com.xiaoningmeng.event;
 
+import com.xiaoningmeng.bean.UserInfo;
+
 public class LoginEvent {
 
+    public UserInfo userInfo;
+    public LoginEvent(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
 }
