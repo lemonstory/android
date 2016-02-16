ac5e89062eeb16c7ce2f52c78e186e67a9802a71
gaoyong <gaoyong06@qq.com>
2016-02-16 18:26:03 +0800

    增加:引用的处理

--- lemon/lemon/src/main/java/com/xiaoningmeng/adapter/ViewThreadAdapter.java
+++ lemon/lemon/src/main/java/com/xiaoningmeng/adapter/ViewThreadAdapter.java
@@ -16,25 +16,34 @@ import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.xiaoningmeng.R;
+import com.xiaoningmeng.ViewThreadActivity;
 import com.xiaoningmeng.bean.Attachment;
 import com.xiaoningmeng.bean.ForumThread;
 import com.xiaoningmeng.bean.Post;
 import com.xiaoningmeng.constant.Constant;
 import com.xiaoningmeng.http.ConstantURL;
 import com.xiaoningmeng.utils.AvatarUtils;
+import com.xiaoningmeng.utils.UiUtils;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.List;
 
 public class ViewThreadAdapter extends BaseAdapter {
+
+    private static final String KEY_QUOTE_IN_ORIGINAL = "quote";
+    private static final String KEY_MESSAGE_IN_ORIGINAL = "message";
+    private static final String KEY_AUTHOR_IN_ORIGINAL = "author";
 
     private LayoutInflater mInflater;
     private Context mContext;
     private List<Post> posts;
     public ForumThread forumThread;
-    public String aaa;
+    private WeakReference<ViewThreadActivity> weak;
 
     public ViewThreadAdapter(Context context,ForumThread forumThread, List<Post> posts) {
 
+        this.weak = new WeakReference<ViewThreadActivity>((ViewThreadActivity)context);
         mContext = context;
         this.forumThread = forumThread;
         this.posts = posts;
@@ -104,26 +113,58 @@ public class ViewThreadAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-//                String fid = posts.get(position).getTid();
-//                Intent i = new Intent(mContext, ForumDisplayActivity.class);
-//                i.putExtra("fid", Integer.parseInt(fid));
-//                i.putExtra("page", 1);
-//                ((BaseFragmentActivity) mContext).startActivityForNew(i);
-            }
-        });
-
         Post post = posts.get(position);
         //TODO:avatartime是要保持一个定值，只有更新头像才变一次,如果每次请求都是新值，cdn的cache就用不上了,每次都会去取源，不够快，费用也会高
         String authorid = post.getAuthorid();
         String avatarTime = String.valueOf(post.getDbdateline());
         String avatarUrl = AvatarUtils.getAvatarUrl(authorid, avatarTime, 120);
+        String original = post.getMessage();
+        final HashMap quoteMessage = this.separateQuoteWithMessage(original);
+        String author = (String) quoteMessage.get("author");
+        String quote = (String) quoteMessage.get("quote");
+        String message = (String) quoteMessage.get("message");
         ImageLoader.getInstance().displayImage(avatarUrl, holder.avatarImg, Constant.AVARAR_OPTIONS);
         holder.authorTV.setText(post.getAuthor());
         holder.datelineTv.setText(Html.fromHtml(post.getDateline()));
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ViewThreadActivity activity = weak.get();
+                boolean isKeyboardVisible = UiUtils.isKeyboardShown(activity.getWindow().getDecorView().getRootView());
+                if(!isKeyboardVisible) {
+
+                    activity.repPid = Integer.parseInt(posts.get(position).getPid());
+                    activity.repPost = Integer.parseInt(posts.get(position).getPid());
+                    /**
+                     * 沿用DZ的规则
+                     * noticetrimstr:[quote][size=2][url=forum.php?mod=redirect&goto=findpost&pid=80&ptid=32][color=#999999]mT 发表于 2016-2-15 14:10[/color][/url][/size]
+                     aaaaa[/quote]
+                     *
+                     */
+                    activity.noticeTrimStr = String.format("[quote][size=2][url=forum.php?mod=redirect&goto=findpost&pid=%s&ptid=%s][color=#999999]%s 发表于 %s[/color][/url][/size]\n" +
+                                    "%s[/quote]",
+                            posts.get(position).getPid(),
+                            posts.get(position).getTid(),
+                            posts.get(position).getAuthor(),
+                            posts.get(position).getDateline(),
+                            (String) quoteMessage.get("message"));
+
+                    String hint = String.format("回复%s:",posts.get(position).getAuthor());
+                    activity.keyBoardfragment.setmEditEmojiconHint(hint);
+                    activity.keyBoardfragment.showKeyboard();
+
+                } else {
+
+                    activity.repPid = 0;
+                    activity.repPost = 0;
+                    activity.noticeTrimStr = null;
+                    activity.keyBoardfragment.resetKeyboard();
+                }
+            }
+        });
+
         if (post.getFirst().equals("1") && post.getPosition().equals("1")) {
 
             //楼主显示回帖数,回帖图标
@@ -155,29 +196,13 @@ public class ViewThreadAdapter extends BaseAdapter {
         // <br />
         // 回帖-2-回复1
 
-        String message = post.getMessage();
-        int divMarkIndex = message.indexOf("<div class=\"reply_wrap\">");
-        if (divMarkIndex != -1) {
+        if(quote != null && !quote.equals("")) {
 
             holder.quoteContainerRl.setVisibility(View.VISIBLE);
-            int fontMarkStartIndex = message.indexOf("<font color=\"#999999\">");
-            int fontMartStartLen = "<font color=\"#999999\">".length();
-            int fontMarkEndIndex = message.indexOf("</font>");
-            String quoteAuthorAndTime =  message.substring(fontMarkStartIndex + fontMartStartLen, fontMarkEndIndex);
-            int blandInQuoteAuthorAndTimeIndex = quoteAuthorAndTime.indexOf(" ");
-            String quoteAuthor = quoteAuthorAndTime.substring(0, blandInQuoteAuthorAndTimeIndex);
-            holder.quoteAuthorTv.setText(quoteAuthor);
-            int brMartFirstIndex = message.indexOf("<br />");
-            int brMartLen = "<br />".length();
-            int divCloseMarkFirstIndex = message.indexOf("</div>");
-            int quoteMessageStartIndex = brMartFirstIndex + brMartLen + 2;
-            String quoteMessage = message.substring(quoteMessageStartIndex,divCloseMarkFirstIndex);
-            holder.quoteMessageTv.setText(Html.fromHtml(quoteMessage));
-            int brMarkSecondIndex = message.indexOf("<br />", divCloseMarkFirstIndex);
-            int messageStartIndex = brMarkSecondIndex + brMartLen + 1;
-            message = message.substring(messageStartIndex);
-
+            holder.quoteAuthorTv.setText(author);
+            holder.quoteMessageTv.setText(Html.fromHtml(quote));
         }else {
+
             holder.quoteContainerRl.setVisibility(View.GONE);
         }
         holder.messageTv.setText(Html.fromHtml(message));
@@ -225,6 +250,41 @@ public class ViewThreadAdapter extends BaseAdapter {
         return convertView;
     }
 
+    private HashMap separateQuoteWithMessage(String original) {
+
+        HashMap<String , String> result = new HashMap<String , String>();
+        result.put(KEY_QUOTE_IN_ORIGINAL,null);
+        result.put(KEY_MESSAGE_IN_ORIGINAL,original);
+        result.put(KEY_AUTHOR_IN_ORIGINAL,null);
+
+        int divMarkIndex = original.indexOf("<div class=\"reply_wrap\">");
+        if (divMarkIndex != -1) {
+
+            int fontMarkStartIndex = original.indexOf("<font color=\"#999999\">");
+            int fontMartStartLen = "<font color=\"#999999\">".length();
+            int fontMarkEndIndex = original.indexOf("</font>");
+            String quoteAuthorAndTime =  original.substring(fontMarkStartIndex + fontMartStartLen, fontMarkEndIndex);
+            int blandInQuoteAuthorAndTimeIndex = quoteAuthorAndTime.indexOf(" ");
+            String quoteAuthor = quoteAuthorAndTime.substring(0, blandInQuoteAuthorAndTimeIndex);
+            result.put(KEY_AUTHOR_IN_ORIGINAL, quoteAuthor);
+
+            int brMartFirstIndex = original.indexOf("<br />");
+            int brMartLen = "<br />".length();
+            int divCloseMarkFirstIndex = original.indexOf("</div>");
+            int quoteMessageStartIndex = brMartFirstIndex + brMartLen + 1;
+            String quoteMessage = original.substring(quoteMessageStartIndex,divCloseMarkFirstIndex);
+            result.put(KEY_QUOTE_IN_ORIGINAL, quoteMessage);
+
+            int brMarkSecondIndex = original.indexOf("<br />", divCloseMarkFirstIndex);
+            int messageStartIndex = brMarkSecondIndex + brMartLen + 1;
+            String message = original.substring(messageStartIndex);
+            result.put(KEY_MESSAGE_IN_ORIGINAL,message);
+        }
+
+        return result;
+    }
+
+
     static class ViewHolder {
 
         FrameLayout containerFl;
@@ -243,9 +303,6 @@ public class ViewThreadAdapter extends BaseAdapter {
         RelativeLayout quoteContainerRl;//引用的容器
         TextView quoteAuthorTv;//引用的发布者名称
         TextView quoteMessageTv;//引用的发布内容
-
         LinearLayout imagesContainerLl; //帖子图片容器
-
-
     }
 }
