8d76cf8c7b161dd3ad32c87cb0ee0f91afd8261d
gaoyong <gaoyong06@qq.com>
2016-02-16 18:23:27 +0800

    增加:键盘，引用回复，加载更多等处理

--- lemon/lemon/src/main/java/com/xiaoningmeng/ViewThreadActivity.java
+++ lemon/lemon/src/main/java/com/xiaoningmeng/ViewThreadActivity.java
@@ -1,11 +1,14 @@
 package com.xiaoningmeng;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baoyz.swipemenu.xlistview.XListView;
 import com.google.gson.Gson;
@@ -14,31 +17,41 @@ import com.xiaoningmeng.adapter.ViewThreadAdapter;
 import com.xiaoningmeng.base.BaseFragmentActivity;
 import com.xiaoningmeng.bean.ForumThread;
 import com.xiaoningmeng.bean.Post;
+import com.xiaoningmeng.constant.Constant;
 import com.xiaoningmeng.fragment.KeyboardFragment;
 import com.xiaoningmeng.http.LHttpHandler;
 import com.xiaoningmeng.http.LHttpRequest;
+import com.xiaoningmeng.utils.UiUtils;
 
 import org.apache.http.Header;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ViewThreadActivity extends BaseFragmentActivity implements XListView.IXListViewListener,KeyboardFragment.OnFragmentInteractionListener {
 
+    private Context mContext;
     private ViewGroup loadingView;
     private XListView mListView;
-    public ViewThreadAdapter mAdapter;
-    private KeyboardFragment keyBoardfragment;
+    public  ViewThreadAdapter mAdapter;
+    public KeyboardFragment keyBoardfragment;
     private ForumThread forumThread = new ForumThread();
     private List<Post> mPosts = new ArrayList<>();
     private String tip = null;
     private View pbEmptyTip;
     private int fid; //论坛ID
     private int tid; //帖子ID
+    private String formHash;//表单验证使用
     private int page; //当前页码
     private int maxPage;//最大页码
+    private int perPage;
+    public int repPid;
+    public int repPost;
+    public String noticeTrimStr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +60,12 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
         setContentView(R.layout.activity_view_thread);
         initView();
 //        this.setTitleName("帖子");
+        mContext = this;
         fid = getIntent().getIntExtra("fid",1);
         tid = getIntent().getIntExtra("tid", 1);
+        formHash = getIntent().getStringExtra("formHash");
         page = getIntent().getIntExtra("page", 1);
+
         mAdapter = new ViewThreadAdapter(this,forumThread,mPosts);
         mListView.setAdapter(mAdapter);
         requestPostsData(tid, page);
@@ -61,35 +77,56 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
         loadingView = (ViewGroup) findViewById(R.id.rl_loading);
         loadingView.setPadding(0, getResources().getDimensionPixelOffset(R.dimen.home_discover_item_img_height), 0, 0);
         mListView.setPullLoadEnable(false);
-        mListView.setFootViewNoMore(true);
-        mListView.setPullRefreshEnable(true);
         mListView.setXListViewListener(this);
         this.page = 1;
         this.maxPage = 1;
         mListView.setFootViewNoMore(true);
         pbEmptyTip = loadingView.findViewById(R.id.pb_empty_tip);
         setKeyBoardFragment(); //设置用户键盘
-        Log.d("aaa", "Fragment by id:" + getSupportFragmentManager().findFragmentById(R.id.fl_keyboard));
-        Log.d("aaa", "Fragment by tag:" + getSupportFragmentManager().findFragmentByTag("keyboardFragment"));
-//        this.keyBoardfragment = (KeyboardFragment) this.getSupportFragmentManager().findFragmentByTag("keyboardFragment");
-//
-//        this.keyBoardfragment.setOnKeyBoardBarViewListener(new KeyboardFragment.KeyBoardBarViewListener() {
-//
-//            @Override
-//            public void onSwitchImgClicked(Button view) {
-//
-//            }
-//
-//            @Override
-//            public void OnSendBtnClick(String msg) {
-//
-//                Log.d("aaa", "ViewThreadActivity OnSendBtnClick is run");
-//                sendForumThreadReplyData(fid, tid, msg);
-//            }
-//
-//        });
 
+        //处理键盘的发送按钮事件
+        getSupportFragmentManager().executePendingTransactions();
+        this.keyBoardfragment = (KeyboardFragment) getSupportFragmentManager().findFragmentByTag("keyboardFragment");
+        if(null != this.keyBoardfragment) {
+
+            this.keyBoardfragment.setOnKeyBoardBarViewListener(new KeyboardFragment.KeyBoardBarViewListener() {
+
+                @Override
+                public void onSwitchImgClicked(Button view) {
+
+                }
+
+                @Override
+                public void OnSendBtnClick(String msg) {
+
+                    sendForumThreadReplyData(fid, tid,formHash,msg);
+                }
+            });
+        }
+
+        //页面滑动时键盘收起
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+                switch (scrollState) {
+                    case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
+
+                        boolean isKeyboardVisible = UiUtils.isKeyboardShown(mListView.getRootView());
+                        if(isKeyboardVisible) {
+                            keyBoardfragment.resetKeyboard();
+                        }
+                        break;
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
     }
+
 
     private void setKeyBoardFragment() {
         getSupportFragmentManager()
@@ -106,7 +143,6 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
         this.page = 1;
         requestPostsData(tid, page);
         mListView.setPullLoadEnable(false);
-        mListView.setFootViewNoMore(true);
     }
 
     @Override
@@ -114,14 +150,17 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
         if(this.page < this.maxPage) {
             int size = mPosts.size();
-            if (size > 0) {
+            if (size >= perPage) {
                 this.page++;
                 requestPostsData(tid, page);
-                ;
             } else {
                 this.page = 1;
                 requestPostsData(tid, page);
             }
+        }else {
+
+            //获取到的数据需要做合并
+            requestPostsData(tid, page);
         }
     }
 
@@ -129,12 +168,10 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
         mListView.stopRefresh();
         mListView.stopLoadMore();
-
+        mListView.setPullLoadEnable(true);
         if(this.page < this.maxPage) {
-            mListView.setPullLoadEnable(true);
             mListView.setFootViewNoMore(false);
-        }else {
-            mListView.setPullLoadEnable(false);
+        } else {
             mListView.setFootViewNoMore(true);
         }
     }
@@ -149,9 +186,15 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
     public void setPosts(List<Post> mPosts) {
 
-
         if (mPosts != null && mPosts.size() > 0) {
-            this.mPosts.addAll(mPosts);
+
+            for (Post post : mPosts) {
+
+                int freq = Collections.frequency(this.mPosts, post);
+                if(freq < 1) {
+                    this.mPosts.add(post);
+                }
+            }
             if (mListView != null) {
                 hideEmptyTip();
                 //TODO:
@@ -192,34 +235,47 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
                     @Override
                     public void onGetDataSuccess(String data) {
+
                         Double replaysCount = 0.0;
-                        Double tpp = 0.0;
+                        Double ppp = 0.0;
+                        ForumThread forumThread = null;
                         try {
+
                             JSONObject jsonObject = new JSONObject(data);
+                            JSONObject variablesObject = new JSONObject(jsonObject.getString("Variables"));
+
                             Gson gson = new Gson();
 
-                            if (jsonObject.has("thread")) {
+                            if (variablesObject.has("thread")) {
 
-                                ForumThread forumThread = gson.fromJson(jsonObject.getString("thread"), new TypeToken<ForumThread>() {
+                                forumThread = gson.fromJson(variablesObject.getString("thread"), new TypeToken<ForumThread>() {
                                 }.getType());
                                 setForumThread(forumThread);
 
                             }
 
-                            if (jsonObject.has("postlist")) {
-                                List<Post> posts = gson.fromJson(jsonObject.getString("postlist"), new TypeToken<List<Post>>() {
+                            if (variablesObject.has("postlist")) {
+
+                                List<Post> posts = gson.fromJson(variablesObject.getString("postlist"), new TypeToken<List<Post>>() {
                                 }.getType());
                                 setPosts(posts);
                             }
 
-                            if (ViewThreadActivity.this.forumThread != null && jsonObject.has("tpp")) {
+                            if(variablesObject.has("formhash")) {
 
-                                tpp = Double.parseDouble(jsonObject.getString("tpp"));
+                                formHash = variablesObject.getString("formhash");
+                            }
+
+                            if (forumThread.getReplies() != null && variablesObject.has("ppp")) {
+
+                                ppp = Double.parseDouble(variablesObject.getString("ppp"));
+                                perPage = Integer.valueOf(variablesObject.getString("ppp"));
 
                                 //TODO:接口里面有两个:replies，allreplies先用前者
-                                if (ViewThreadActivity.this.forumThread.getReplies() != null) {
-                                    replaysCount = Double.parseDouble(ViewThreadActivity.this.forumThread.getReplies());
-                                    Double dMaxPage = Math.ceil(replaysCount / tpp);
+                                if (forumThread.getReplies() != null) {
+                                    //+1是因为还有主贴
+                                    replaysCount = Double.parseDouble(forumThread.getReplies()) + 1;
+                                    Double dMaxPage = Math.ceil(replaysCount / ppp);
                                     ViewThreadActivity.this.maxPage = dMaxPage.intValue();
                                 }
                             }
@@ -227,6 +283,7 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
                         } catch (JSONException e) {
 
+                            e.printStackTrace();
                         }
                     }
 
@@ -263,7 +320,7 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
     }
 
-    private void sendForumThreadReplyData(int fid, int tid,String message) {
+    private void sendForumThreadReplyData(int fid, int tid,String formHash, String message) {
 
         if (loadingView != null) {
 
@@ -275,32 +332,39 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
 
                     @Override
                     public void onGetDataSuccess(String data) {
-                        Double threadsCount = 0.0;
-                        Double tpp = 0.0;
                         try {
-                            JSONObject jsonObject = new JSONObject(data);
-                            Log.d("aaa", data);
-//                            if (jsonObject.has("forum_threadlist")) {
-//                                Gson gson = new Gson();
-//                                List<ForumThread> threads = gson.fromJson(jsonObject.getString("forum_threadlist"), new TypeToken<List<ForumThread>>() {
-//                                }.getType());
-//                                setForumsThreads(threads);
-//                            }
 
-//                            if (jsonObject.has("forum") && jsonObject.has("tpp")) {
-//
-//                                String forum = jsonObject.getString("forum");
-//                                JSONObject forumJsonObject = new JSONObject(forum);
-//                                tpp = Double.parseDouble(jsonObject.getString("tpp"));
-//
-//                                if (forumJsonObject.has("threadcount")) {
-//                                    threadsCount = Double.parseDouble(forumJsonObject.getString("threadcount"));
-//                                    Double dMaxPage = Math.ceil(threadsCount / tpp);
-//                                    ForumDisplayActivity.this.maxPage = dMaxPage.intValue();
-//                                }
-//                            }
+                            JSONObject jsonObject = new JSONObject(data);
+                            JSONObject messageObject = new JSONObject(jsonObject.getString("Message"));
+                            String messageVal = messageObject.getString("messageval");
+                            String messageStr = messageObject.getString("messagestr");
+
+                            if(messageVal.equals(Constant.FORUM_POST_REPLY_SUCCEED)) {
+
+                                //关闭键盘
+                                keyBoardfragment.resetKeyboard();
+
+                                //TODO:待优化
+                                //可见的最后一条与实际全量的最后一条距离相近时,页面向下滚动
+                                int lastVisiblePosition =  mListView.getLastVisiblePosition();
+                                int maxPosition = mPosts.size();
+                                if(maxPosition - lastVisiblePosition < 4) {
+                                    onLoadMore();
+                                    //页面滚动到最后
+                                    mListView.smoothScrollToPosition(mPosts.size() - 1);
+                                }else {
+                                    onLoadMore();
+                                    Toast.makeText(mContext, "发送成功",Toast.LENGTH_SHORT).show();
+                                }
+
+                            }else {
+
+                                Toast.makeText(mContext,messageStr,Toast.LENGTH_SHORT).show();
+                            }
+
                         } catch (JSONException e) {
 
+                            e.printStackTrace();
                         }
                     }
 
@@ -329,8 +393,10 @@ public class ViewThreadActivity extends BaseFragmentActivity implements XListVie
                         onLoad();
                         super.onFinish();
                     }
-                }, fid, tid, message);
+                }, fid, tid,formHash, message,repPid,repPost,noticeTrimStr);
     }
+
+
 
 
 
