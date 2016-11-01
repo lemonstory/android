d400a249cf0f949ee60aa653990a42218ddb745f
e <873825232@qq.com>
2016-11-01 13:23:42 +0800

    1.增加缓存拦截器和重写网络回调

---
+++ lemon/lemon/src/main/java/com/xiaoningmeng/http/CacheInterceptor.java
@@ -0,0 +1,45 @@
+package com.xiaoningmeng.http;
+
+import com.xiaoningmeng.application.MyApplication;
+import com.xiaoningmeng.utils.NetUtils;
+
+import java.io.IOException;
+
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by huang on 2016/10/22.
+ * 缓存拦截器 可以配置
+ * 没有网络状态 使用缓存
+ * 有网络的情况下
+ */
+
+public class CacheInterceptor implements Interceptor{
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        if(!NetUtils.isNetworkConnected(MyApplication.getContext())){
+            request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build();
+        }
+        Response originalResponse = chain.proceed(request);
+        if(NetUtils.isNetworkConnected(MyApplication.getContext())){
+            //有网的时候读接口上的@Headers里的配置，你可以在这里进行统一的设置
+            String cacheControl = request.cacheControl().toString();
+            return originalResponse.newBuilder()
+                    .header("Cache-Control", cacheControl)
+                    .removeHeader("Pragma")
+                    .build();
+        }else{
+            return originalResponse.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=2419200")
+                    .removeHeader("Pragma")
+                    .build();
+        }
+
+    }
+}
